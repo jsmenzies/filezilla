5f5bc57de1df11ab2c9d4d54bd8945a1c85bdf9d
James Menzies <james@menzies.io>
2023-05-14 17:30:57 +1000

    Started metadata

--- src/image.rs
+++ src/image.rs
@@ -1,13 +1,10 @@
 use std::path::PathBuf;
 
-use time::OffsetDateTime;
-
 #[derive(Debug)]
 pub struct Image {
     path: PathBuf,
     filename: String,
     name: String,
-    filename_dt: OffsetDateTime,
 }
 
 #[derive(Default)]
@@ -15,7 +12,6 @@ pub struct ImageBuilder {
     path: Option<PathBuf>,
     filename: Option<String>,
     name: Option<String>,
-    filename_dt: Option<OffsetDateTime>,
 }
 
 impl ImageBuilder {
@@ -24,7 +20,6 @@ impl ImageBuilder {
             path: None,
             filename: None,
             name: None,
-            filename_dt: None,
         }
     }
 
@@ -43,22 +38,16 @@ impl ImageBuilder {
         self
     }
 
-    pub fn filename_dt(mut self, filename_dt: OffsetDateTime) -> Self {
-        self.filename_dt = Some(filename_dt);
-        self
-    }
 
     pub fn build(self) -> Result<Image, String> {
         let path = self.path.ok_or("Missing path")?;
         let filename = self.filename.ok_or("Missing filename")?;
         let name = self.name.ok_or("Missing name")?;
-        let filename_dt = self.filename_dt.ok_or("Missing filename_dt")?;
 
         Ok(Image {
             path,
             filename,
             name,
-            filename_dt,
         })
     }
 }
--- src/jpg.rs
+++ src/jpg.rs
@@ -1,16 +1,20 @@
+use std::fs;
 use std::path::{Path, PathBuf};
 
+use chrono::{DateTime, NaiveDate, Utc};
 use color_eyre::eyre::eyre;
-use regex::Regex;
-use time::{Date, OffsetDateTime};
+use color_eyre::Report;
+use regex::{Captures, Match, Regex};
 
 use crate::image;
 
-pub fn process(files: &mut Vec<PathBuf>) -> Result<(), color_eyre::Report> {
+pub fn process(files: &mut Vec<PathBuf>) -> Result<(), Report> {
     println!("processing {} jpg files", files.len());
 
+    let mut success = 0;
+    let mut failed = 0;
+
     for file in files {
-        // cut off first 5 characters of the filename
         let filename = file
             .file_name()
             .and_then(|n| n.to_str())
@@ -19,45 +23,53 @@ pub fn process(files: &mut Vec<PathBuf>) -> Result<(), color_eyre::Report> {
 
         let name = String::from(&filename[5..]);
 
-        let image = image::ImageBuilder::new()
-            .name(name.clone())
-            .path(file.clone())
-            .filename(filename.clone())
-            .filename_dt(OffsetDateTime::now_utc())
-            .build().unwrap();
+        let date = date_from_filename(&name);
 
-        println!("{:?}", image);
-
-
-        // let date = date_from_filename(file)?;
-        // println!("{}: {}", file.display(), date);
+        if let Err(err) = date {
+            println!("{}", err);
+            failed += 1;
+        }
     }
+    println!("{} files processed, {} failed", success, failed);
+
     Ok(())
 }
 
-fn date_from_filename(photo: &Path) -> Result<OffsetDateTime, color_eyre::Report> {
-    let file_name = photo
-        .file_name()
-        .ok_or_else(|| eyre!("failed to retrieve photo filename"))?;
+fn datetime_from_exif(photo: &Path) -> Result<DateTime<Utc>, Report> {
+    todo!()
+}
 
-    // let img_dt = Regex::new(r"^IMG[-_](\d{4})(\d{2})\d{2}[-_](WA)?\d+\.(jpeg|jpg|JPG)$").unwrap();
-    // let dt = Regex::new(r"(?x)(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})").unwrap();
+fn date_from_filename(name: &String) -> Result<NaiveDate, Report> {
+    // let dt = Regex::new(r"^IMG[-_](\d{4})(\d{2})\d{2}[-_](WA)?\d+\.(jpeg|jpg|JPG)$").unwrap();
+    // let dt = Regex::new(r"^(?x)(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})").unwrap();
+    let dt = Regex::new(r"^(?:IMG-|IMG_|Screenshot_|VideoCapture_)?(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})").unwrap();
+    // let dt = Regex::new(r"(?:IMG-)(\d{4})(\d{2})(\d{2})_(\d{6})(\(\d\))?").unwrap();
 
-    // let captures = self
-    //     .date_from_filename_regex
-    //     .captures(
-    //         file_name
-    //             .to_str()
-    //             .ok_or_else(|| eyre!("failed to get file name as string"))?,
-    //     )
-    //     .ok_or_else(|| eyre!("file name doesn't have date format"))?;
-    // let year: u16 = match captures.get(1) {
-    //     Some(y) => y.as_str().parse().unwrap(),
-    //     None => return Err(eyre!("failed to retrieve year from filename")),
-    // };
-    // let month: u8 = match captures.get(2) {
-    //     Some(m) => m.as_str().parse().unwrap(),
-    //     None => return Err(eyre!("failed retrieve month from filename")),
-    // };
-    Ok(OffsetDateTime::now_utc())
+    let captures: Option<Captures> = dt.captures(name.as_str());
+
+    if let Some(caps) = captures {
+        let year = check_date_in_range(caps.get(1), 2000, 2022);
+        let month = check_date_in_range(caps.get(2), 1, 12);
+        let day = check_date_in_range(caps.get(3), 1, 31);
+
+        return if let (Ok(y), Ok(m), Ok(d)) = (year, month, day) {
+            let date = NaiveDate::from_ymd_opt(y, m as u32, d as u32).unwrap();
+            println!("{}: {}", name, date);
+            Ok(date)
+        } else {
+            Err(eyre!("ERROR Date format Y/M/D: {:?}", name))
+        };
+    }
+    Err(eyre!("ERROR Date regex fail: {:?}", name))
+}
+
+fn check_date_in_range(opt: Option<Match<>>, lower: i32, upper: i32) -> Result<i32, Report> {
+    if let Some(value) = opt {
+        if let Ok(parsed_val) = value.as_str().parse::<i32>() {
+            if lower <= parsed_val && parsed_val <= upper {
+                return Ok(parsed_val);
+            }
+        }
+    }
+    Err(eyre!("ERROR Date component fail: {:?}", opt))
 }
